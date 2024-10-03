3a63efeafd55ef9d9762e90298b4d3eb14ccba8a
tofickbrodaga <mshdtlv@gmail.com>
2024-10-03 22:12:28 +0300

    Flake8 for classes

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
+"""Initialize & added API method's for bot."""
+from typing import Optional
+from uuid import UUID
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
-from uuid import UUID, uuid4
-from typing import Optional
+
 
 class Meme(BaseModel):
+    """Model for API methods."""
+
     comment: str = Field(..., max_length=100)
     photo: str = Field(...)
     visibility: Optional[bool] = Field(default=False)
 
+
 class ChangeVisibility(BaseModel):
+    """Model for API methods."""
+
     meme_id: UUID
     visibility: bool
-
