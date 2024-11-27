a7a55347481c102885a5e7ab371127a0ee52d232
rTiRe <zientenin@mail.ru>
2024-11-28 01:54:15 +0300

    Update image upload logic

--- bot/src/handlers/main_menu.py
+++ bot/src/handlers/main_menu.py
@@ -7,20 +7,19 @@ from src.states.states import MainStates
 from src.templates.env import render
 from src.keyboards.main_menu import keyboard
 from config.settings import settings
-from src.utils.upload_image import upload_image
+from src.utils.image import Image
 
 
 async def main_menu(message: Message, state: FSMContext) -> None:
     await message.delete()
     await state.set_state(MainStates.main_menu)
     user_profile_photo = await message.from_user.get_profile_photos()
-    user_photo_id = user_profile_photo.photos[0][2].file_id
-    bot_files_url = f'https://api.telegram.org/file/bot{settings.BOT_TOKEN}/'
-    file_path = (await message.bot.get_file(user_photo_id)).file_path
+    user_photo_id = user_profile_photo.photos[0][-1].file_id
+    file_url = await Image.get_telegram_url(user_photo_id)
     message = await message.answer(
         text=render(
             template_name='main_menu.jinja2',
-            image_url=await upload_image(f'{bot_files_url}{file_path}'),
+            image_url=await Image.get_public_url(file_url),
             user_username=message.from_user.username,
             user_id=message.from_user.id,
         ),
---
+++ bot/src/utils/image.py
@@ -0,0 +1,45 @@
+from aiohttp import ClientSession
+from typing import Union
+from types import MappingProxyType
+
+from config.settings import settings
+from src.bot import get_bot
+
+
+class Image:
+    __URL = 'https://freeimage.host/api/1/upload'
+    __PARAMS = MappingProxyType(
+        {
+            'key': settings.IMAGE_HOST_API_KEY,
+            'format': 'json',
+        },
+    )
+
+    @classmethod
+    async def upload(
+        cls,
+        session: ClientSession,
+        **kwargs,
+    ) -> Union[dict, None]:
+        async with session.get(cls.__URL, **kwargs) as response:
+            if response.status == 200:
+                return await response.json()
+            return None
+
+    @classmethod
+    async def get_public_url(cls, source: str) -> str:
+        url = ''
+        async with ClientSession() as session:
+            this_params = cls.__PARAMS.copy()
+            this_params['source'] = source
+            response = await cls.upload(session, params=this_params)
+            if response:
+                url = response['image']['display_url']
+        return url
+
+    @classmethod
+    async def get_telegram_url(cls, telegram_file_id: str) -> str:
+        bot = get_bot()
+        bot_files_url = f'https://api.telegram.org/file/bot{settings.BOT_TOKEN}'
+        file_path = (await bot.get_file(telegram_file_id)).file_path
+        return f'{bot_files_url}/{file_path}'
